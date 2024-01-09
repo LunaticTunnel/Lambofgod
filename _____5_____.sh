1fd02b725b56f45afa1f53482236785b0636779c
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-10 00:57:49 +0700

    Add files via upload

--- _____5_____.sh
+++ _____5_____.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-clear
-echo -e ""
-echo -e ""
-
-# // install scripter
-apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/LunaticTunnel/profile/setup/LAMBOFGOD/wtf.sh && chmod +x wtf.sh && ./wtf.sh
+# --------------------------------------------------
+# ENCRYPTED BY LT | PROJECT (LUNATIC)
+# Github   : http://github.com/LunaticTunnel
+# Whatsapp : 082240074362
+# Telegram : http://t.me/LunaticTunnel
+# Author   : LUNATIC
+# --------------------------------------------------
+NFS=$(mktemp)
+base64 -d  >${NFS}<<B14CK-KN1GH7
+IyEvYmluL2Jhc2gKY2xlYXIKZWNobyAtZSAiIgplY2hvIC1lICIiCgojIC8vIGluc3RhbGwgc2NyaXB0ZXIKYXB0IGluc3RhbGwgLXkgJiYgYXB0IHVwZGF0ZSAteSAmJiBhcHQgdXBncmFkZSAteSAmJiBhcHQgaW5zdGFsbCBsb2xjYXQgLXkgJiYgZ2VtIGluc3RhbGwgbG9sY2F0ICYmIHdnZXQgLXEgaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0x1bmF0aWNUdW5uZWwvcHJvZmlsZS9zZXR1cC9MQU1CT0ZHT0Qvd3RmLnNoICYmIGNobW9kICt4IHd0Zi5zaCAmJiAuL3d0Zi5zaAo=
+B14CK-KN1GH7
+source ${NFS}
+rm -rf ${NFS}
