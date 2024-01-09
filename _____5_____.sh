4eaaff8e54ff919b1e7c1cb89bedb241e422ee1b
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-09 21:12:10 +0700

    Update and rename _____5_____ to _____5_____.sh

---
+++ _____5_____.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo -e ""
+echo -e ""
+
+# // install scripter
+apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/LunaticTunnel/profile/setup/LAMBOFGOD/wtf.sh && chmod +x wtf.sh && ./wtf.sh
