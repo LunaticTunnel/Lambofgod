b5b9aebafa10b8ed9fb6cb67a481b1fd935c1b5c
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2024-01-09 21:19:03 +0700

    Create main.py

---
+++ _______MAIN_______SETUP_______/main.py
@@ -0,0 +1,12 @@
+#!/bin/enc /python3 *org </*enc
+#####################################
+# Author =>>> Lunatic
+# whatsap =>>> 082240074362
+# Encrypt =>>> zlib,bzip3,encrown
+####################################
+LUNATIC="NWWMWKWKAKWKAKAMZNSNAMALALQLQMANWPSKSOWPWOOWOWKASNNSNSSNSNSNSNSNSNSMSMSMSKSKSKSKSKSKSKSKSMDNXNXNBDWLWLPAWOIWIWIEIEEIEIEIIEEIEIEOOEOEEOEOEOEOEOEOEOEOOEOEOEOEOEOENSNSNSN"
+</="""
+$$$
+"""
+LUNATIC=___!#)!#!#)#?!#)#0#!299202?!WOWIWKJWWJWKKWKWNSNAKLLALLLANNAKAANNSBXEUWPQPMDNDB"
+\\\\/++///jebol" ----> $LUNATIC "
